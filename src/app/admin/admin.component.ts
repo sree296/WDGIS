a81964afe8e0a8661fe1d0c41460e53e6e6f2a91
sree296 <nivas296@gmail.com>
2018-08-04 13:08:54 +0530

    top 5 users

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -7,7 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  topUsers : any[];
+
+  constructor() {
+    this.topUsers = [
+      {
+        "name" : "Mark Mary",
+        "scanCount" : 211
+      },
+      {
+        "name" : "Mary John",
+        "scanCount" : 201
+      },
+      {
+        "name" : "John Mark",
+        "scanCount" : 189
+      },
+      {
+        "name" : "Chris Heli",
+        "scanCount" : 164
+      },
+      {
+        "name" : "David Siej",
+        "scanCount" : 142
+      }
+    ]
+   }
 
   ngOnInit() {
   }
