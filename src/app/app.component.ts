6a5c4e67e78b87f116e89b5a214ed85411bf8206
sree296 <nivas296@gmail.com>
2018-08-07 09:19:26 +0530

    passing validAdmin user variable from parent to nav component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  //public adminUser = false;
+  public adminUser = true;
 }
--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-nav-menu',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavMenuComponent implements OnInit {
 
+  @Input() public parentData;
+  
   constructor() { }
 
   ngOnInit() {
